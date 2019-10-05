b330e43ed44adafbb8a6c308c77d697fd0f5beed
曹锐 <caoruister@gmail.com>
2019-10-05 15:05:41 +0800

    update composer.lock

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\EasySmsServiceProvider::class,
-
+        Dingo\Api\Provider\LaravelServiceProvider::class,
     ],
 
     /*
